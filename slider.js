61a1ef285b863f79999cf946aba0a7804b0bc075
Mohammad Fardeen <31559901+mfardeen9@users.noreply.github.com>
2023-11-25 18:32:03 +0530

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,73 @@
+const left = document.querySelector(".arrowleft");
+const right = document.querySelector(".arrowright");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
+
+let slideNumber = 1;
+const length = images.length;
+
+for( let i=0; i< length; i++){
+    const div = document.createElement("div");
+    div.className = "button";
+    bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons[0].style.backgroundColor ="white";
+
+const resetBackground = ()=>{
+    buttons.forEach((button)=>{
+        button.style.backgroundColor = 'transparent';
+    });
+};
+
+buttons.forEach((button,i)=>{
+    button.addEventListener("click", ()=>{
+        resetBackground();
+        slider.style.transform = `translateX(-${i*800}px)`;
+        slideNumber= i+ 1;
+        button.style.backgroundColor = 'white';
+    });
+});
+
+const changeColor = ()=>{
+    resetBackground()
+    buttons[slideNumber-1].style.backgroundColor = "white";
+  }
+
+const nextSlide = () => {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+  slideNumber++;
+};
+
+const getFirstSlide = () => {
+  slider.style.transform = `translateX(-0px)`;
+  slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
+  slideNumber < length ? nextSlide() : getFirstSlide();
+  changeColor();
+});
+
+
+
+
+const getLastSlide = () =>{
+    slider.style.transform= `translateX(-${(length -1 ) * 800}px)`;
+    slideNumber=length;
+}
+
+const prevSlide = () =>{
+    slider.style.transform = `translateX(-${(slideNumber -2) * 800}px)`
+    slideNumber--;
+}
+
+
+left.addEventListener("click", () => {
+  slideNumber > 1 ? prevSlide() : getLastSlide();
+  changeColor();
+});
+
+
